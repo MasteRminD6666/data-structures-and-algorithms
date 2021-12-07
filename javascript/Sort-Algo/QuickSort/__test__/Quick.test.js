5b35453d3b00b4113650507c0e957157aff2fec3
MasteRminD6666 <rami-zaitoun99@hotmail.com>
2021-12-07 16:38:05 +0200

    solved

---
+++ javascript/Sort-Algo/QuickSort/__test__/Quick.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { partition, quickSortIterative, quickSortRecursive } = require('../QuickSort');
+
+describe('it should test sort method', () => {
+
+    it('tests quick sort iterative', () => {
+        let arr = [7, -2, 4, 1, 6, 5, 0, -4, 2];
+        quickSortIterative(arr);
+        expect(arr).toEqual([-4, -2, 0, 1, 2, 4, 5, 6, 7]);
+        console.log(arr);
+    });
+
+    it('tests quick sort recursive', () => {
+        let arr = [7, -2, 4, 1, 6, 5, 0, -4, 2];
+        quickSortRecursive(arr);
+        expect(arr).toEqual([-4, -2, 0, 1, 2, 4, 5, 6, 7]);
+        console.log(arr);
+    });
+
+    it('tests partition', () => {
+        let arr = [7, -2, 4, 1, 6, 5, 0, -4, 2];
+        partition(arr);
+        expect(arr).toEqual([7, -2, 4, 1, 6, 5, 0, -4, 2]);
+        console.log(arr);
+    });
+
+});
