a65797ce14f5189a36b0cd001ac916d77754b5eb
MasteRminD6666 <rami-zaitoun99@hotmail.com>
2021-12-14 00:14:28 +0200

    adding

--- javascript/hashtable/README3.md
+++ javascript/hashtable/README3.md
@@ -1,1 +1,33 @@
 # Code Challenges
+# Hashmap Tree Intersection
+
+## Challenge
+
+- Write a function called tree intersection
+  - It takes two `binary trees`
+  - It returns an array of common values between the two trees
+
+## Whiteboard Process
+
+![challenge-32]()
+
+## Efficiency
+
+### Efficiency
+
+- Space :
+  - O(n)
+- Time :
+  - O(n)
+
+## API
+
+### How to Use
+
+- To check for intersections between two trees :
+  - `<name-of-hash-map>.treeIntersection(<tree1>, <tree2>)`
+
+### Test
+stuck with the testing part 
+
+## got helped form someone on github and refactoring his code  
--- javascript/hashtable/__test__/hashtable.test.js
+++ javascript/hashtable/__test__/hashtable.test.js
@@ -1,5 +1,7 @@
 'use strict';
 const Hashmap = require('../Hashtable.js');
+const BinaryTree = require('../../trees/index').BinaryTree
+const Node = require('../../trees/index').Node;
 describe('Hash Map', () => {
     test('should create a hash map with pre defined size', () => {
         const hashMap = new Hashmap(5000);
@@ -69,3 +71,44 @@ describe('Hash Map Repeated Words', () => {
       expect(hashMap.repeatedWord(case3)).toBe('hard');
     });
   });
+// TODO: check the test again 
+  // describe('Binary Tree Insertion', () => {
+  //   const one = new Node(1);
+  //   const two = new Node(2);
+  //   const three = new Node(3);
+  //   const four = new Node(4);
+  //   const five = new Node(5);
+  //   const six = new Node(6);
+  //   const seven = new Node(7);
+  //   const one2 = new Node(1);
+  //   const two2 = new Node(2);
+  //   const three2 = new Node(3);
+  //   const four2 = new Node(4);
+  //   const seven2 = new Node(7);
+  //   const eight = new Node(8);
+  //   const nine = new Node(9);
+  //   const tree1 = new BinaryTree(one);
+  //   one.left = two;
+  //   one.right = three;
+  //   two.left = four;
+  //   two.right = five;
+  //   three.left = six;
+  //   three.right = seven;
+  //   const tree2 = new BinaryTree(one2);
+  //   one2.left = two2;
+  //   one2.right = three2;
+  //   two2.left = four2;
+  //   two2.right = eight;
+  //   three2.left = nine;
+  //   three2.right = seven2;
+  
+  //   test('Find common values in 2 binary trees.', () => {});
+  //   const expected = [1, 2, 4, 3, 7];
+  //   const hashMap = new Hashmap(4000);
+  //   const arr = hashMap.treeIntersection(tree1, tree2);
+  //   expect(arr[0]).toBe(expected[0]);
+  //   expect(arr[1]).toBe(expected[1]);
+  //   expect(arr[2]).toBe(expected[2]);
+  //   expect(arr[3]).toBe(expected[3]);
+  //   expect(arr[4]).toBe(expected[4]);
+  // });
